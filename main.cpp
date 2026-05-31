c7cbc22c59c0fa41f6c5b5a76208a0b8ffa65a99
Kittisak 'Froy' Porkha <kittijustsaid@gmail.com>
2026-05-31 21:46:12 +0700

    update another ways to solve

--- main.cpp
+++ main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 #include <stdexcept>
 #include <string>
+
+using namespace std;
+
 //
 //
 // for the question 
@@ -11,16 +14,26 @@
 // output 28
 //
 // exp: the sum between  first to n 
-int sum(int n) {
 
-  if (n == 1)
-    return 1;
+int sum(int n) {
+  if (n <= 1)
+    return n;
 
   return n + sum(n-1);
 }
 
-int main () {
+//second way to approach
+
+int fact(int n) {
+  if (n <= 0)
+    return 1;
+
+  return n * fact(n-1);
+}
+
+int main() {
   int n = 5;
-  cout << sum(n);
+  cout << "sum of first " << n << " is: " << sum(n) << endl;
+  cout << "factorial of 5 : " << fact(5) << endl;
   return 0;
 }
