a2197406b92b9fc524b0375ec2072e78e437ec2a
Kittisak 'Froy' Porkha <kittijustsaid@gmail.com>
2026-06-09 10:38:26 +0700

    update'

--- book.cpp
+++ book.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <stdexcept>
+#include <string.h>
 
 // exercise 4.1: write the program to print my name:
 //
@@ -13,12 +14,34 @@
 
 // exercise 4.3
 //
+// int main() {
+//   float pi = 3.14;
+//   double r;
+//   std::cout << "input your radius: ";
+//   std::cin >> r;
+//   double outputs = 2 * pi * r;
+//   std::cout << outputs;
+//   return 0;
+// }
+//
+
+// string
+
 int main() {
-  float pi = 3.14;
-  double r;
-  std::cout << "input your radius: ";
-  std::cin >> r;
-  double outputs = 2 * pi * r;
-  std::cout << outputs;
-  return 0;
+  // declaration
+  int oldNumber;
+  int Number;
+  int NextNumber;
+
+  // set the number
+  oldNumber = 1;
+  Number = 1;
+
+  while (Number < 100) {
+    NextNumber = Number + oldNumber;
+    std::cout << Number << std::endl;
+
+    oldNumber = Number;
+    Number = NextNumber;
+  }
 }
